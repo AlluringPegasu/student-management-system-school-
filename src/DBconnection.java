77a4016e57bb7430029fe184da6bd4a06832a90e
Amal Jose Plackal <100516409+AlluringPegasu@users.noreply.github.com>
2026-09-06 21:04:59 +0530

    Clarify database connection URL in DBconnection.java
    Updated connection URL comment to clarify database setup.

--- src/DBconnection.java
+++ src/DBconnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 
 public class DBconnection {
     public static Connection getConnection(String user, String password) throws Exception {
-        String url = "jdbc:oracle:thin:@localhost:1521/FREEPDB1"; 
+        String url = "jdbc:oracle:thin:@localhost:1521/FREEPDB1";  //change it according to your database setup
         return DriverManager.getConnection(url, user, password);
     }
 }
